bc1095324d0f6175c51fc5042171d94019ade2f1
laura-paullette <laura.paullette@student.moringaschool.com>
2023-05-22 12:24:25 +0300

    completed

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@genres = []
+    @@count = 0
+    @@artists = []
+
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def Song.count
+        @@count
+    end
+
+    def Song.genres
+        @@genres.uniq
+    end
+
+    def Song.artists
+        @@artists.uniq
+    end
+
+    def Song.genre_count
+        @@genres.tally
+    end
+
+    def Song.artist_count
+        @@artists.tally
+    end
+
+
+
+
+
+
+
+
+
+end
